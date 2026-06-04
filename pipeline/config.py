5c7193bf0fef858ba4bf081bb75e66fba916f91a
4chad <109893050+4chad@users.noreply.github.com>
2026-06-04 16:42:29 +0530

    Add files via upload

---
+++ pipeline/config.py
@@ -0,0 +1,9 @@
+# DO NOT push to GitHub — add to .gitignore
+
+DB_CONFIG = {
+    "host":     "localhost",
+    "port":     5432,
+    "database": "ipl",
+    "user":     "postgres",
+    "password": "Sanskar%40549sandy"
+}
